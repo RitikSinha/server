d922893f5f77eeb4221b9f9555a32619da947ed3
Ritik Sinha <45168546+RitikSinha@users.noreply.github.com>
2022-02-18 23:46:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const Gun = require("gun");
 
 const app = express();
+app.get("/", (req, res) => {
+  res.send("relay server");
+});
 app.use(Gun.serve);
 const PORT = process.env.port || 8080;
 const server = app.listen(PORT, () => console.log(`listening on ${PORT}`));
